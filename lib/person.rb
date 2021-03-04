3b28dc3801040fc49c763e4d88f50ce65df08007
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-03-04 06:30:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
 class Person 
-  def initialize(breed)
-    @breed = breed
+  def initialize(name)
+    @name = name
   end
-*  
-*   def breed=(breed)
-*     @breed = breed
-*   end
-*  
-*   def breed
-*     @breed
-*   end
+ 
+   def name=(name)
+     @name = name
+   end
+ 
+   def name
+     @name
+   end
 end
