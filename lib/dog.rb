017c09a4a56def5d561eda4092ff2924db35e21d
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-03-04 06:31:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
-* def initialize(breed)
-*     @breed = breed
-*   end
-*  
-*   def breed=(breed)
-*     @breed = breed
-*   end
-*  
-*   def breed
-*     @breed
-*   end
+class Dog
+  def initialize(breed)
+   @breed = breed
+  end
+
+  def breed=(breed)
+   @breed = breed
+  end
+  
+  def breed
+   @breed
+  end
+end
